3a0a156015715933a783d1ae0a3049bbbd2f8f80
Lidii <lidiiany92@gmail.com>
2020-04-06 18:50:18 -0700

    more test cases

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -2,11 +2,28 @@ describe('Browser Actions', () => {
 	it('should load books website', () => {
 		cy.visit('http://books.toscrape.com/index.html');
 		cy.url().should('include', 'index.html');
+		cy.log('Website loaded');
 	});
 
 	it('should click on travel category', () => {
 		cy.get('a').contains('Travel').click();
 
 		cy.get('h1').contains('Travel');
+	});
+
+	it('should display correct number of books', () => {
+		cy.get('.product_pod').its('length').should('equal', 11);
+	});
+
+	it('should click on poetry category', () => {
+		cy.get('a').contains('Poetry').click();
+	});
+
+	it('should click on Olio book', () => {
+		cy.get('a').contains('Olio').click();
+	});
+
+	it('should display correct price', () => {
+		cy.get('.price_color').contains('£23.88');
 	});
 });
