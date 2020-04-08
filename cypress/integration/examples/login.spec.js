f74d0ed2126156cd0652265fbfb32737a40376c3
Lidii <lidiiany92@gmail.com>
2020-04-08 16:41:32 -0700

    changing a date to the specific date

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -1,6 +1,17 @@
 describe('Working with inputs', () => {
+	it('should overwrite the current time', () => {
+		//Returns a time stamp
+		const date = new Date(2020, 3, 10).getTime();
+		cy.clock(date);
+		cy.log(date);
+	});
+
 	it('should load login page', () => {
 		cy.visit('http://zero.webappsecurity.com/login.html');
+
+		//Clearing cookie if needed
+		cy.clearCookies({ log: true });
+		cy.clearLocalStorage('your items in local storage', { log: true });
 	});
 
 	it('should fill username', () => {
@@ -25,6 +36,8 @@ describe('Working with inputs', () => {
 	});
 
 	it('should display error message', () => {
-		cy.get('.alert-error').should('be.visible');
+		cy.get('.alert-error')
+			.should('be.visible')
+			.and('contain', 'Login and/or password are wrong');
 	});
 });
