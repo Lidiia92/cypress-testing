8df9fb4e74a9345b4465623db82be80da05c8aac
Lidii <lidiiany92@gmail.com>
2020-04-13 19:16:00 -0700

    reading data from the website

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -6,4 +6,19 @@ describe('Write / Read data to JSON / Text file', () => {
 	it('should write data into text file', () => {
 		cy.writeFile('log.txt', 'Test text');
 	});
+
+	it('should read and verify data from JSON file', () => {
+		cy.readFile('log.json').its('age').should('eq', 25);
+	});
+
+	it('should read and verify data from text file', () => {
+		cy.readFile('log.txt').should('eq', 'Test text');
+	});
+
+	it('should read and verify browser document content', () => {
+		cy.visit('https://example.com');
+		cy.wait(2000);
+		cy.document().its('contentType').should('eq', 'text/html');
+		cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+	});
 });
